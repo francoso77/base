ebc7ff60faf4d03d863035efed1db5bd347d2669
francoso77 <frank77invest@gmai.com>
2023-07-27 16:38:59 -0300

    tabela pessoas

--- src/views/Person/Person.tsx
+++ src/views/Person/Person.tsx
@@ -12,6 +12,7 @@ import { GlobalContext, GlobalContextInterface } from '../../Context/GlobalConte
 import { ActionInterface, actionTypes } from '../../interfaces/ActionInterface';
 import { MensagemTipo } from '../../Context/MensagemState';
 import ComText from '../../Components/ComText';
+import DataTable, { DataTableCabecalhoInterface } from '../../Components/DataTable';
 
 
 const TEMPO_REFRESH_TEMPORARIO = 500
@@ -21,69 +22,72 @@ interface PesquisaInterface {
 }
 
 export default function Person() {
-  const globalContext = (useContext(GlobalContext) as GlobalContextInterface)
+
+  const Cabecalho: Array<DataTableCabecalhoInterface> = [
+    {
+      campo: 'name',
+      cabecalho: 'Nome',
+      alinhamento: 'left'
+    },
+    {
+      campo: 'category',
+      cabecalho: 'Categoria',
+      alinhamento: 'left',
+    }
+  ]
+
+  const ResetDados: PersonInterface = {
+    idPerson: 0,
+    name: '',
+    category: 0
+  }
+
   const { mensagemState, setMensagemState } = useContext(GlobalContext) as GlobalContextInterface
   const [localState, setLocalState] = useState<ActionInterface>({ action: actionTypes.pesquisando })
   const [rsPesquisa, setRsPesquisa] = useState<Array<PersonInterface>>([])
   const [pesquisa, setPesquisa] = useState<PesquisaInterface>({ nome: '' })
-  const [person, setPerson] = useState<PersonInterface>({
-    idPerson: 0,
-    name: "",
-    category: 0
-  })
+  const [person, setPerson] = useState<PersonInterface>(ResetDados)
 
-  const federacoes = [
-    'Federação Brasileira',
-    'Federação Carioca',
-    'Federação Mineira',
-    'Federação Paulista'
-  ]
+  const categorys = ['Despesas', 'Receitas']
 
-  const [federacao, setFederacao] = React.useState('');
-
-  const handleChangeFederacao = (event: SelectChangeEvent) => {
-    setFederacao(event.target.value as string);
+  const handleChangeCategory = (event: SelectChangeEvent) => {
+    let cat: number = parseInt(event.target.value as string)
+    setPerson({ ...person, category: cat })
   };
 
-  const printTable = () =>
-    rsPesquisa.map((person) =>
-      <tr key={person.idPerson}>
-        <td>{person.name}</td>
-        <td>{person.category}</td>
-        <td>
-          <input type="button" value="Editar" onClick={() => btEditar(person.idPerson, actionTypes.editando)} />
-          <input type="button" value="Excluir" onClick={() => btEditar(person.idPerson, actionTypes.excluindo)} />
-        </td>
-      </tr>
-    )
+  // const printTable = () =>
+  //   rsPesquisa.map((person) =>
+  //     <tr key={person.idPerson}>
+  //       <td>{person.name}</td>
+  //       <td>{person.category}</td>
+  //       <td>
+  //         <input type="button" value="Editar" onClick={() => btEditar(person.idPerson, actionTypes.editando)} />
+  //         <input type="button" value="Excluir" onClick={() => btEditar(person.idPerson, actionTypes.excluindo)} />
+  //       </td>
+  //     </tr>
+  //   )
 
-  const btEditar = (id: number, ac: string) => {
+  const btEditar = (idPerson: number, action: string) => {
     setMensagemState({ ...mensagemState, exibir: true, mensagem: 'Pesquisando Pessoa', tipo: MensagemTipo.Info, modal: true })
-
     setTimeout(() => {
 
-      fetch(URL_SERVIDOR.concat('/person/'.concat(id.toString())), {
+      fetch(URL_SERVIDOR.concat('/person/'.concat(idPerson.toString())), {
         headers: {
           'Content-Type': 'application/json',
         },
         method: 'GET'
       }).then(rs => {
+        if (rs.ok) {
+          setMensagemState({ ...mensagemState, exibir: false, mensagem: '', modal: false })
 
-        // Primeiro Then do Fetch - Testo Status + Tratamento dos dados
-
-        if (rs.status == 200) {
-          setMensagemState({ ...mensagemState, exibir: false, mensagem: '', tipo: MensagemTipo.Ok, modal: false })
-
-          // Envio somente os dados para o próximo Then....
           return rs.json()
 
         } else {
           setMensagemState({ ...mensagemState, exibir: true, mensagem: 'Erro ao pesquisa Pessoa', tipo: MensagemTipo.Info, modal: true })
         }
-      }).then(rsPessoa => {
-
-        setPerson(rsPessoa)
-        setLocalState({ action: actionTypes.pesquisando })
+      }).then(rsPerson => {
+        setPerson(rsPerson)
+        setLocalState({ action: action })
 
       }).catch(() => {
         setMensagemState({ ...mensagemState, exibir: true, mensagem: 'Erro na conexão, não foi possível fazer a exclusão!', tipo: MensagemTipo.Error, modal: true })
@@ -117,13 +121,9 @@ export default function Person() {
 
         if (rs.ok) {
 
-          setMensagemState({ ...mensagemState, exibir: true, mensagem: 'Pessoa excluída com sucesso', tipo: MensagemTipo.Ok, modal: true })
+          setMensagemState({ ...mensagemState, exibir: true, mensagem: 'Pessoa excluída com sucesso', tipo: MensagemTipo.Info, modal: true })
 
-          setPerson({
-            idPerson: 0,
-            name: "",
-            category: 0
-          })
+          setPerson(ResetDados)
 
           setLocalState({ action: 'pesquisando' })
 
@@ -159,17 +159,13 @@ export default function Person() {
       }).then(rs => {
         if (rs.ok) {
 
-          setPerson({
-            idPerson: 0,
-            name: "",
-            category: 0
-          })
+          setPerson(ResetDados)
 
           setLocalState({ action: 'pesquisando' })
 
           btPesquisar()
 
-          setMensagemState({ ...mensagemState, exibir: true, mensagem: 'Dados alterados com sucesso', tipo: MensagemTipo.Ok, modal: true })
+          setMensagemState({ ...mensagemState, exibir: true, mensagem: 'Dados alterados com sucesso', tipo: MensagemTipo.Info, modal: true })
         } else {
           setMensagemState({ ...mensagemState, exibir: true, mensagem: 'Erro na alteração', tipo: MensagemTipo.Error, modal: true })
         }
@@ -191,14 +187,10 @@ export default function Person() {
         },
         method: 'POST'
       }).then(rs => {
-        if (rs.status === 201) {
+        if (rs.ok) {
 
-          setPerson({
-            idPerson: 0,
-            name: "",
-            category: 0
-          })
-          setMensagemState({ ...mensagemState, exibir: true, mensagem: 'Pessoa incluída com sucesso', tipo: MensagemTipo.Ok, modal: true })
+          setPerson(ResetDados)
+          setMensagemState({ ...mensagemState, exibir: true, mensagem: 'Pessoa incluída com sucesso', tipo: MensagemTipo.Info, modal: true })
         } else {
           setMensagemState({ ...mensagemState, exibir: true, mensagem: 'Erro na inclusão', tipo: MensagemTipo.Error, modal: true })
         }
@@ -215,28 +207,23 @@ export default function Person() {
     setTimeout(() => {
 
       fetch(URL_SERVIDOR.concat('/person?name_like='.concat(pesquisa.nome)), {
-        // body: JSON.stringify(escola),
         headers: {
           'Content-Type': 'application/json',
         },
         method: 'GET'
       }).then(rs => {
 
-        // Primeiro Then do Fetch - Testo Status + Tratamento dos dados
+        if (rs.ok) {
+          setMensagemState({ ...mensagemState, exibir: false, mensagem: '', tipo: MensagemTipo.Info, modal: false })
 
-        if (rs.status === 200) {
-          setMensagemState({ ...mensagemState, exibir: false, mensagem: '', tipo: MensagemTipo.Ok, modal: false })
-
-          // Envio somente os dados para o próximo Then....
           return rs.json()
 
         } else {
           setMensagemState({ ...mensagemState, exibir: true, mensagem: 'Erro ao pesquisar', tipo: MensagemTipo.Error, modal: true })
         }
-      }).then(rsEscolas => {
+      }).then((rsPerson: PersonInterface[]) => {
 
-        console.log(JSON.stringify(rsEscolas))
-        setRsPesquisa(rsEscolas)
+        setRsPesquisa(rsPerson)
 
       }).catch(() => {
         setMensagemState({ ...mensagemState, exibir: true, mensagem: 'Erro na conexão, não foi possível fazer a pesquisa!', tipo: MensagemTipo.Error, modal: true })
@@ -286,31 +273,29 @@ export default function Person() {
                   <Button variant='contained' onClick={() => btIncluir()}>Incluir</Button>
 
                 </Grid>
-
-
-
               </>
             }
 
             {localState.action !== 'pesquisando' &&
               <>
                 <Grid item xs={12} sm={10} mt={3}>
-                  <ComText label="Nome" type="text" dados={person} field="nome" setState={setPerson} disabled={localState.action == 'excluindo' ? true : false} />
+                  <ComText label="Nome" type="text" dados={person} field="name" setState={setPerson} disabled={localState.action == 'excluindo' ? true : false} />
                 </Grid>
 
-                <Grid item xs={12} sm={3} mt={5}>
+                <Grid item xs={12} sm={10} mt={3}>
                   <Box>
                     <FormControl fullWidth>
-                      <InputLabel id="demo-simple-select-label">Federação</InputLabel>
+                      <InputLabel id="demo-simple-select-label">Categoria</InputLabel>
                       <Select
                         labelId="demo-simple-select-label"
                         id="demo-simple-select"
-                        value={federacao}
-                        label="Federação"
-                        onChange={handleChangeFederacao}
+                        value={person.category.toString()}
+                        label="Categoria"
+                        onChange={handleChangeCategory}
+                        sx={{ padding: 0, margin: 0 }}
                       >
-                        {federacoes.map((federacao, i) => (
-                          <MenuItem key={i} value={i}>{federacao}</MenuItem>
+                        {categorys.map((category, i) => (
+                          <MenuItem key={i} value={i}>{category}</MenuItem>
                         ))}
 
                       </Select>
@@ -322,38 +307,46 @@ export default function Person() {
             }
 
             {localState.action === 'incluindo' &&
-              <Button variant="contained" onClick={btConfirmarInclusao}>Confirmar Inclusão</Button>
+              <Button sx={{ marginTop: 3 }} variant="contained" onClick={btConfirmarInclusao} >Confirmar Inclusão</Button>
             }
 
             {localState.action === 'editando' &&
-              <Button variant="contained" onClick={btConfirmarEdicao}>Confirmar Edição</Button>
+              <Button sx={{ marginTop: 3 }} variant="contained" onClick={btConfirmarEdicao} >Confirmar Edição</Button>
             }
 
             {localState.action === 'excluindo' &&
-              <Button variant="contained" onClick={btConfirmarExclusao}>Confirmar Exclusão</Button>
+              <Button sx={{ marginTop: 3 }} variant="contained" onClick={btConfirmarExclusao} >Confirmar Exclusão</Button>
             }
 
             {localState.action !== 'pesquisando' &&
-              <Button variant="contained" onClick={btCancelar}>Cancelar</Button>
+              <Button sx={{ marginTop: 3, marginLeft: 1 }} variant="contained" onClick={btCancelar}>Cancelar</Button>
             }
 
             {
               localState.action === 'pesquisando' &&
 
-              <table className='clsTableEscola'>
-                <thead>
-                  <tr>
-                    <td>Nome</td>
-                    <td>Categoria</td>
-                    <td>Ações</td>
-                  </tr>
-                </thead>
-                <tbody>
-                  {
-                    printTable()
-                  }
-                </tbody>
-              </table>
+              // <table className='clsTableEscola'>
+              //   <thead>
+              //     <tr>
+              //       <td>Nome</td>
+              //       <td>Categoria</td>
+              //       <td>Ações</td>
+              //     </tr>
+              //   </thead>
+              //   <tbody>
+              //     {
+              //       printTable()
+              //     }
+              //   </tbody>
+              // </table>
+              <Grid item xs={12} sx={{ mt: 3 }}>
+                <DataTable
+                  dados={rsPesquisa}
+                  cabecalho={Cabecalho}
+                  onEditar={() => btEditar(person.idPerson, actionTypes.editando)}
+                  onExcluir={() => btEditar(person.idPerson, actionTypes.excluindo)}
+                />
+              </Grid>
             }
           </Grid>
         </Paper>
