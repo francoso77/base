2e0a170e62a520e37e0941f9f1d9e19f62a41b8f
francoso77 <frank77invest@gmai.com>
2023-08-21 16:01:08 -0300

    incluíndo a classe

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Box, Grid, Button, TextField } from '@mui/material';
+import { CalculadoraCls } from '../Utils/CalculadoraCls';
 
 const teclado: string[] = [
   'x²', '√', '←', '/',
@@ -10,14 +11,28 @@ const teclado: string[] = [
 ]
 
 export default function Home() {
+
+  const passaValor = (tecla: string) => {
+    var calc = new CalculadoraCls();
+    calc.enviaValor(tecla)
+  }
+
   return (
     <>
-      <TextField id="txtVisor" name="txtVisor" variant="outlined" />
+      <TextField sx={{ width: '98%', margin: 2 }} id="txtVisor" name="txtVisor" variant="outlined" disabled />
       <Box sx={{ flexGrow: 1 }}>
-        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 12, sm: 12, md: 12 }}>
+        <Grid container spacing={{ xs: 1, md: 1 }} columns={{ xs: 12, sm: 12, md: 12 }}>
           {teclado.map((tecla, index) => (
             <Grid item xs={3} sm={3} md={3} key={index}>
-              <Button>
+              <Button
+                sx={{
+                  height: '100%',
+                  width: '100%',
+                  background: '#49c4ff',
+                  color: '#000077',
+                }}
+                onClick={() => passaValor(tecla)}
+              >
                 {tecla}
               </Button>
             </Grid>
