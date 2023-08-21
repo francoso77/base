2500a4b3b5a64f9f3b81b5b2907d551d73c4c0f3
francoso77 <frank77invest@gmai.com>
2023-08-21 15:23:50 -0300

    up

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import * as React from 'react';
+import { Box, Grid, Button, TextField } from '@mui/material';
 
-function Home() {
+const teclado: string[] = [
+  'x²', '√', '←', '/',
+  '7', '8', '9', '*',
+  '4', '5', '6', '+',
+  '1', '2', '3', '-',
+  'C', '0', ',', '='
+]
+
+export default function Home() {
   return (
-    <div>
-      <h1>
-        oiiiii
-      </h1>
-    </div>
+    <>
+      <TextField id="txtVisor" name="txtVisor" variant="outlined" />
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 12, sm: 12, md: 12 }}>
+          {teclado.map((tecla, index) => (
+            <Grid item xs={3} sm={3} md={3} key={index}>
+              <Button>
+                {tecla}
+              </Button>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+    </>
   );
 }
-
-export default Home;
